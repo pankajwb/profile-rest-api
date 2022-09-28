52a213fce25149af1a81acce6732ef9fde2bc31b
Pankaj <pankajwb@gmail.com>
2022-09-28 11:45:10 +0530

    Minor in file path

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/pankajwb/profile-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/'
+PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
 # Set Ubuntu Language
